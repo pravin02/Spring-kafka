a3c09ddbd1461a4db7462ac1c1c76f9a0ef9eaa0
pravin02 <prvnpatil11@gmail.com>
2022-12-21 12:46:42 +0530

    consumer created to poll messages

--- kafka-consumer/src/main/java/org/pk/kakfa/kafkaconsumer/KafkaConsumer.java
+++ kafka-consumer/src/main/java/org/pk/kakfa/kafkaconsumer/KafkaConsumer.java
@@ -1,2 +1,18 @@
-package org.pk.kakfa.kafkaconsumer;public class KafkaConsumer {
+package org.pk.kakfa.kafkaconsumer;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+
+    private static final Logger LOGGER = LogManager.getLogger(KafkaConsumerApplication.class);
+
+    @KafkaListener(topics = "test", groupId = "1")
+    public void receiveMessage(String message) {
+        LOGGER.info("Message: " + message);
+    }
+
 }
