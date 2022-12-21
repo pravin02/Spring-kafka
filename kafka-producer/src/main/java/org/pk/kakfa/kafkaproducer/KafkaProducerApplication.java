752ff0b8149be22d5723a3c06a7bbf48928ca152
pravin02 <prvnpatil11@gmail.com>
2022-12-21 12:45:58 +0530

    producer application class update

--- kafka-producer/src/main/java/org/pk/kakfa/kafkaproducer/KafkaProducerApplication.java
+++ kafka-producer/src/main/java/org/pk/kakfa/kafkaproducer/KafkaProducerApplication.java
@@ -1,28 +1,16 @@
 package org.pk.kakfa.kafkaproducer;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.annotation.KafkaListener;
 
 @EnableKafka
 @SpringBootApplication
 public class KafkaProducerApplication {
-    private static final Logger LOGGER = LogManager.getLogger(KafkaProducerApplication.class);
-
-    @Value("spring.kafka.topic-name")
-    private String topicName;
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaProducerApplication.class, args);
-    }
-
-    @KafkaListener(topics = "test", groupId = "1")
-    public void receiveMessage(String message) {
-        LOGGER.info("Received Message " + message);
     }
 
 }
